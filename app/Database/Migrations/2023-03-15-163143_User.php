40dd144107ea611fd76f55135a129fd05e544302
kumhar98 <pradeepkumhar52@gmail.com>
2023-03-16 14:00:42 +0530

    'Commit'

---
+++ app/Database/Migrations/2023-03-15-163143_User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class User extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'first_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
---
+++ app/Views/welcome_message.php
@@ -0,0 +1,3 @@
+<?php 
+  echo "pradeep kumar";
+?>
